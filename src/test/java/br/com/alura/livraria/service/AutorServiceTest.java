691b177f35cbbe983ac5cf4dc818994bb9905567
leobonvini <leonardobonvini@id.uff.br>
2021-10-25 03:01:49 -0300

    testes unitarios das classes service

--- src/test/java/br/com/alura/livraria/service/AutorServiceTest.java
+++ src/test/java/br/com/alura/livraria/service/AutorServiceTest.java
@@ -1,14 +1,46 @@
 package br.com.alura.livraria.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.com.alura.livraria.dto.AutorDTO;
+import br.com.alura.livraria.dto.AutorFormDTO;
+import br.com.alura.livraria.repository.AutorRepository;
+
+@ExtendWith(MockitoExtension.class)
 class AutorServiceTest {
+	
+	@Mock
+	private AutorRepository autorRepository;
+	
+	@InjectMocks
+	private AutorService autorService;
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void deveriaCadastrarUmAutor() {
+		AutorFormDTO formDTO = new AutorFormDTO(
+				"Leonardo", 
+				"leonardo@leonardo.com",
+				LocalDate.of(1997, 5, 28),
+				"Livros Genericos"
+				);
+		
+		AutorDTO dto = autorService.cadastrar(formDTO);
+		
+		Mockito.verify(autorRepository).save(Mockito.any());
+		
+		assertEquals(formDTO.getNome(), dto.getNome());
+		assertEquals(formDTO.getEmail(), dto.getEmail());
+		assertEquals(formDTO.getMiniCurriculo(), dto.getMiniCurriculo());
+		
 	}
 
 }
--- src/test/java/br/com/alura/livraria/service/LivroServiceTest.java
+++ src/test/java/br/com/alura/livraria/service/LivroServiceTest.java
@@ -1,14 +1,67 @@
 package br.com.alura.livraria.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDate;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.com.alura.livraria.dto.LivroDTO;
+import br.com.alura.livraria.dto.LivroFormDTO;
+import br.com.alura.livraria.repository.AutorRepository;
+import br.com.alura.livraria.repository.LivroRepository;
+
+@ExtendWith(MockitoExtension.class)
 class LivroServiceTest {
+	
+	@Mock
+	private AutorRepository autorRepository;
+	
+	@Mock
+	private LivroRepository livroRepository;
+	
+	@InjectMocks
+	private LivroService livroService;
+	
+	private LivroFormDTO criarFormDTO() {
+		LivroFormDTO formDTO = new LivroFormDTO(
+				"Livro",
+				LocalDate.of(2021, 01, 01),
+				100,
+				1l);
+		return formDTO;
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void deveriaCadastrarUmLivro() {
+		LivroFormDTO formDTO = criarFormDTO();
+		
+		LivroDTO dto = livroService.cadastrar(formDTO);
+		
+		Mockito.verify(livroRepository).save(Mockito.any());
+		
+		assertEquals(formDTO.getTitulo(), dto.getTitulo());
+		assertEquals(formDTO.getLancamento(), dto.getLancamento());
+		assertEquals(formDTO.getNumeroDePaginas(), dto.getNumeroDePaginas());
+		
 	}
+	
+	void naoDeveriaCadastrarUmLivroComAutorInvalido() {
+		LivroFormDTO formDTO = criarFormDTO();
+		
+		Mockito.when(autorRepository.getById(formDTO.getAutorId()))
+		.thenThrow(EntityNotFoundException.class);
+		
+		assertThrows(IllegalArgumentException.class, () -> livroService.cadastrar(formDTO));
+	}
+	
 
 }
